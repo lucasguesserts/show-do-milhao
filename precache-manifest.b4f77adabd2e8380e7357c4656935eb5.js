5cffba9adbc4b6bf15f3685b7834f49d3888f620
Lucas Guesser <github.process990@passinbox.com>
2025-06-16 22:12:34 -0300

    Updates

---
+++ precache-manifest.b4f77adabd2e8380e7357c4656935eb5.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee588db29472f4d12d3f79f73bc8a7cc",
+    "url": "/show-do-milhao/index.html"
+  },
+  {
+    "revision": "8cd21106921dacd2bef7",
+    "url": "/show-do-milhao/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "2cf9a7cf63ab0dbf4570",
+    "url": "/show-do-milhao/static/css/main.12c0597d.chunk.css"
+  },
+  {
+    "revision": "8cd21106921dacd2bef7",
+    "url": "/show-do-milhao/static/js/2.32a7f2ea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/show-do-milhao/static/js/2.32a7f2ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf9a7cf63ab0dbf4570",
+    "url": "/show-do-milhao/static/js/main.d214abb3.chunk.js"
+  },
+  {
+    "revision": "2f1622160c4394e075f3",
+    "url": "/show-do-milhao/static/js/runtime-main.048b6b7d.js"
+  },
+  {
+    "revision": "afa908bcc85d729e4aa4ab2ed786cf2f",
+    "url": "/show-do-milhao/static/media/background.afa908bc.png"
+  },
+  {
+    "revision": "258c6a2f74069e67618d7055fc12ff7b",
+    "url": "/show-do-milhao/static/media/cartas.258c6a2f.mp3"
+  },
+  {
+    "revision": "f5936e8b95d6e1ba748477ceaf56b69d",
+    "url": "/show-do-milhao/static/media/certa_resposta.f5936e8b.mp3"
+  },
+  {
+    "revision": "cd46933f858196d15fe76456e77e7f41",
+    "url": "/show-do-milhao/static/media/chupar_dedo.cd46933f.ogg"
+  },
+  {
+    "revision": "3a65ec6bf6344af6bce8f58984a0cd7b",
+    "url": "/show-do-milhao/static/media/errada_resposta.3a65ec6b.mp3"
+  },
+  {
+    "revision": "9a2bfa5bea9beba97273c1e29b93a290",
+    "url": "/show-do-milhao/static/media/ganhou_chocolatao.9a2bfa5b.mp3"
+  },
+  {
+    "revision": "e783a4ce19381ccdcaa62601dad1831a",
+    "url": "/show-do-milhao/static/media/ganhou_chocolate.e783a4ce.mp3"
+  },
+  {
+    "revision": "32de996eabcc3373df481f97bbd2761b",
+    "url": "/show-do-milhao/static/media/ganhou_chocolatinho.32de996e.mp3"
+  },
+  {
+    "revision": "0aa4dd9b754d9ad7cebdd1decc30cd1f",
+    "url": "/show-do-milhao/static/media/home.0aa4dd9b.mp3"
+  },
+  {
+    "revision": "1d864be0eb91d015ab69ebb8ea19d7c1",
+    "url": "/show-do-milhao/static/media/inicio_pergunta.1d864be0.mp3"
+  },
+  {
+    "revision": "cdd25a54a6e13854e1f1762ea2b1ab9f",
+    "url": "/show-do-milhao/static/media/logo.cdd25a54.png"
+  },
+  {
+    "revision": "e037cc4ad5756e5326a504a72d2dd13f",
+    "url": "/show-do-milhao/static/media/pesquisa.e037cc4a.mp3"
+  },
+  {
+    "revision": "4921469c70c4662db2313d033ca3005e",
+    "url": "/show-do-milhao/static/media/plateia.4921469c.mp3"
+  },
+  {
+    "revision": "2f5733b50db3ff2868effef705a4a916",
+    "url": "/show-do-milhao/static/media/pular.2f5733b5.mp3"
+  },
+  {
+    "revision": "cd9550bed4a2f4ce431ae5194b6bc8fd",
+    "url": "/show-do-milhao/static/media/tempo_acabou.cd9550be.mp3"
+  },
+  {
+    "revision": "5f86a254386f3aa968ba5cee8f29d95e",
+    "url": "/show-do-milhao/static/media/tic_toc.5f86a254.mp3"
+  }
+]);
